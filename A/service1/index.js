10407710c387708eaa0c44c92e313c0bdc433382
Sourabh Kale <sourabhkale@outlook.com>
2024-07-23 12:01:34 +0530

    updated service A

--- A/service1/index.js
+++ A/service1/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Service 1');
+  res.send('Hello from Service A-1');
 });
 
 app.listen(port, () => {
